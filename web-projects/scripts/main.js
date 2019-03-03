6592511f6d6af9f73c1d31f99c58d5de6683ed6b
mariaauntora <48170701+mariaauntora@users.noreply.github.com>
2019-03-03 22:19:33 +0600

    Update main.js

--- web-projects/scripts/main.js
+++ web-projects/scripts/main.js
@@ -1,20 +1,14 @@
 var myImage = document.querySelector('img');
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/pic2..jpg') 
-      myImage.setAttribute ('src','images/pic6.jpg');
-     else if(mySrc === 'images/pic6.jpg')
+    if(mySrc === 'images/ori.jpg') 
+      myImage.setAttribute ('src','images/picss.jpg');
+     else if(mySrc === 'images/picss.jpg')
       myImage.setAttribute ('src','images/picc.jpg');
       else if(mySrc === 'images/picc.jpg')
       myImage.setAttribute ('src','images/picc1.jpg');
-      else if(mySrc === 'images/picc1.jpg')
-      myImage.setAttribute ('src','images/picc2.jpg');
-      else if(mySrc === 'images/picc2.jpg')
-      myImage.setAttribute ('src','images/picss.jpg');
-      else if(mySrc === 'images/picss.jpg')
-      myImage.setAttribute ('src','images/pix.jpg');
       else
-      myImage.setAttribute ('src','images/pic2..jpg');
+      myImage.setAttribute ('src','images/ori.jpg');
     }
     var myButton = document.getElementById('change');
     var myHeading = document.querySelector('p');
